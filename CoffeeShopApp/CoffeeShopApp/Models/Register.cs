a3588e574223a664cbac85eaf8b880e8e99e32b4
Unknown <itegrootenhuis@bizstream.com>
2018-02-26 20:40:02 -0500

    Init app

---
+++ CoffeeShopApp/CoffeeShopApp/Models/Register.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Models
+{
+    public class Register
+    {
+        [Required]
+        public string FirstName     { get; set; }
+
+        [Required]
+        public string LastName      { get; set; }
+
+        [Required]
+        public string Email         { get; set; }
+
+        [Required]
+        [StringLength(15)]
+        public string PhoneNumber   { get; set; }
+
+        [Required]
+        public string Password      { get; set; }
+
+        public string Gender        { get; set; }
+        public string Education     { get; set; }
+    }
+}
